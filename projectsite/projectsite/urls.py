9dc1ebaa91311b9981d04af2b521f9cd8f8f8497
markdavid <uzumakib094@gmail.com>
2026-03-06 13:10:30 +0800

    Fixed views, templates, and URL patterns for Organization CRUD

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization- delete'),
 ]
